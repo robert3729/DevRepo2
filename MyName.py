6f7108cf9d57efc23a63b96b4df404a1c918a592
Kent Hervey <kent.hervey@gmail.com>
2023-12-21 15:05:30 -0600

    added more lines

--- MyName.py
+++ MyName.py
@@ -1,6 +1,20 @@
 usertext = input("What is your name? ")
 print("Hello", usertext)
 
+# create variable named newVar and assign it the value of 5
+
+newVar = 5
+
+# create another variable and assign it the value of 10
+
+newVar2 = 10
+
+# print the sum of newVar and newVar2
+sum = newVar + newVar2
+
+print(sum)
+
+
 
 
 
